8874c740a291fa2858d4bfad52b864672961cbda
Andrey Filonov <andrey@MacBook3.local>
2018-11-22 14:49:23 +1300

    Example/Pods added

---
+++ Example/Pods/Target Support Files/Clocket/Clocket-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Clocket : NSObject
+@end
+@implementation PodsDummy_Clocket
+@end
---
+++ Example/Pods/Target Support Files/Pods-Clocket_Example/Pods-Clocket_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Clocket_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Clocket_Example
+@end
